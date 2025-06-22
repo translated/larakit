dfcbd24827ca17c055bea72829f0dd9ea9022d6a
Davide Caroselli <davide.caroselli@translated.net>
2025-06-22 16:50:05 +0200

    Fix set()

--- larakit/_core.py
+++ larakit/_core.py
@@ -57,43 +57,28 @@ class Namespace:
 class StatefulNamespace(Namespace):
     def __init__(self, path: str, autosave: bool = False, **default_kwargs):
         super().__init__()
-        self._path = path
-        self._autosave = autosave
+
+        for key, value in default_kwargs.items():
+            super().set(key, value)
+
+        object.__setattr__(self, '_path', path)
+        object.__setattr__(self, '_autosave', autosave)
 
         if os.path.isfile(self._path):
             with open(self._path, 'r', encoding='utf-8') as f_input:
                 data = json.load(f_input)
                 for key, value in data.items():
                     super().set(key, value)
-        else:
-            for key, value in default_kwargs.items():
-                super().set(key, value)
 
         if self._autosave:
             self.save()
 
-    def __setattr__(self, key, value):
-        self.set(key, value)
-
     def set(self, key, value):
         super().set(key, value)
+
         if self._autosave:
             self.save()
 
     def save(self, indent: int = 2, sort_keys: bool = True):
-        print('SAVE')
         with open(self._path, 'w', encoding='utf-8') as f_output:
             f_output.write(json.dumps(self.to_json(), indent=indent, sort_keys=sort_keys))
-
-
-if __name__ == '__main__':
-    # Example usage
-    # ns = StatefulNamespace('/Users/davide/workspace/larakit/test.json', key1='value1', key2={'inner': 'value2'}, list1=['item1', 'item2'])
-    ns = StatefulNamespace('/Users/davide/workspace/larakit/test.json', autosave=True)
-    ns._private_value = 'private'
-
-    print(ns)
-    ns.new_value = 'new_value'
-    print(ns)
-
-    print(json.dumps(ns.to_json()))
